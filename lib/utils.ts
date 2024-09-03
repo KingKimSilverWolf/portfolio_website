b481b25857bcfaeeac6d9eb2f6ff61ce99aa2595
KingKimSilverWolf <129137737+KingKimSilverWolf@users.noreply.github.com>
2024-09-03 10:40:50 -0400

    chore: Update project title and description in layout.tsx and globals.css

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+// Helper function that helps everything work together with tailwind CSS
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
